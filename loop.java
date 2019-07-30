f5f37171da0be63472bee165aaa756b76cbb966b
Sameer <sameerhankare@gmail.com>
2019-07-30 14:59:11 +0530

    Initial Commit

---
+++ loop.java
@@ -0,0 +1,10 @@
+public class hello{
+	static void printhello(int b){
+		for(int i=o;i<b;i++)
+			System.out.println("hello");
+	}
+	public static void main(String args[])
+	{
+		printhello(5);
+	}
+}
